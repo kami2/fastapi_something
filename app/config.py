a41fdd5639ed28da14f6d80f8c6c6f0917e8af81
Kamil <kami.krasowski.uk@gmail.com>
2024-08-08 20:28:44 +0100

    Enhanced config with dotenv Added gitignore

--- app/config.py
+++ app/config.py
@@ -1,16 +1,27 @@
 import logging
+from dotenv import load_dotenv
+import os
+
+
+def get_config(key: str):
+    logging.info(f"Get environment variable {key}")
+    return os.environ.get(key)
 
 
 class Config:
 
+    load_dotenv()
+
     CAMERA_HOST_IP = "192.168.42.1"
     CAMERA_HOST_PORT = 7878
     CAMERA_RTSP_ADDRESS = "rtsp://192.168.42.1/live"
+    TEST_VAR = os.environ.get("TEST_VAR")
 
     @staticmethod
     def initialize():
         try:
             logging.basicConfig(format='%(asctime)s: %(levelname)s: %(message)s', encoding='utf-8', level=logging.DEBUG)
+            logging.info("CONFIG INITIALIZED")
         except Exception as e:
             raise f"Failed to initialize config [{e}]"
 

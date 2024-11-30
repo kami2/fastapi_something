0acae316bac79f3f9598ad606488cda31415c16b
Kamil <kami.krasowski.uk@gmail.com>
2024-11-30 20:43:40 +0000

    Updated slack_helper.py; Added send_file functionality

--- fastapi_something/helpers/slack_helper.py
+++ fastapi_something/helpers/slack_helper.py
@@ -16,7 +16,14 @@ class SlackHelper:
         except SlackApiError as e:
             logging.info(f"ERROR: {e}")
 
+    def send_file(self, channel_id: str, file_path: str, text: str = None):
+        try:
+            self.client.files_upload_v2(channel=channel_id, file=file_path, initial_comment=text)
+        except SlackApiError as e:
+            logging.info(f"ERROR: {e}")
+
 
 if __name__ == "__main__":
     paciak_slack = SlackHelper()
-    paciak_slack.send_message(channel="#testowanko_na_produkcji", text="Send from API")
+    # paciak_slack.send_message(channel="#testowanko_na_produkcji", text="Debug")
+    paciak_slack.send_file(channel_id="C01L22NNZPY", file_path="./cache/norzxr.jpg", text="Do i need to provide channel id rather than channel name? Why this shit is fine for send message but not for file upload? Norbert cisnie")
